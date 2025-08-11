8dfae121e8f85b319c4ed585a53521de69d9b1e0
makefilmmade <ehrha44@gmail.com>
2025-08-11 21:02:53 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";              // ← default import (확실히)
+import App from "./App"; // ← 반드시 default import
 
-import "./index.css";                 // 있으면 유지
+import "./index.css"; // 있으면 유지
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
