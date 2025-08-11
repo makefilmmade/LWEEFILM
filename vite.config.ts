12b9975bf1fe2c9db2170a80849922e08e118912
makefilmmade <ehrha44@gmail.com>
2025-08-11 20:19:34 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/lweefilm-website/" : "/",
+  base: mode === "production" ? "/LWEEFILM/" : "/", 
   server: { host: "::", port: 8080 },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
