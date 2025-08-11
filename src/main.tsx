6550cf6187bc56e4277fda8ce47065459d0580f5
makefilmmade <ehrha44@gmail.com>
2025-08-11 20:57:22 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-createRoot(document.getElementById("root")!).render(<App />);
+import { BrowserRouter } from "react-router-dom";
+// ...
+<BrowserRouter basename={import.meta.env.BASE_URL}>
+  <App />
+</BrowserRouter>
