aab7c3af6fd500a84efd0e68090420c2c48f59a6
makefilmmade <ehrha44@gmail.com>
2025-08-11 21:26:22 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 import App from "./App";
+// import "./index.css"; // 쓰고 계시면 유지
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
