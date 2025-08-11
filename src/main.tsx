ada14c3e969a25b94eeae2df8bc68d9cb00c0b8b
makefilmmade <ehrha44@gmail.com>
2025-08-11 21:09:22 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App"; // ← 반드시 default import
+import App from "./App";
 
-import "./index.css"; // 있으면 유지
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/LWEEFILM">
+<BrowserRouter basename={import.meta.env.BASE_URL}>
   <App />
 </BrowserRouter>
-  </React.StrictMode>
-);
