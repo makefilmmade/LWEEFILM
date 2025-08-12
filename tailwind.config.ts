7cac0aa316c5643797f96bf66b5f3206657436e5
makefilmmade <ehrha44@gmail.com>
2025-08-12 12:37:41 +0900

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,11 @@
 import type { Config } from "tailwindcss";
 
 export default {
-	darkMode: ["class"],
-	content: [
-		"./pages/**/*.{ts,tsx}",
-		"./components/**/*.{ts,tsx}",
-		"./app/**/*.{ts,tsx}",
-		"./src/**/*.{ts,tsx}",
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
+  plugins: [require("@tailwindcss/typography")],
+} satisfies Config;
 	],
 	prefix: "",
 	theme: {
