c9a1fc3be112b529fae06471406697bb8a1132a3
makefilmmade <ehrha44@gmail.com>
2025-08-12 12:34:33 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
   base: mode === "production" ? "/LWEEFILM/" : "/",
-  server: { host: "::", port: 8080 },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  server: { host: "::", port: 8080 },
 }));
