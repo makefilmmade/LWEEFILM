84399e20f4d0431e2f940452b4895c4c999ba864
makefilmmade <ehrha44@gmail.com>
2025-08-12 12:36:25 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// ✅ 전역 스타일 (Tailwind 포함) 반드시 임포트
+// 전역 스타일
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
